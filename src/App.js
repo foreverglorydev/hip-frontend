43bf548216bed9d3d90a22da3c2e31aa705a4209
it718 <vytruslan719@gmail.com>
2022-02-07 18:55:40 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/home'));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/shop" element={<Home />} />
+          <Route path="/mint" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
